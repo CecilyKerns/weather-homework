c67bc3afde688a4927d997610e01ff8fe5f7748d
Cecily Kerns <cecilyrose2002@gmail.com>
2019-11-25 18:26:03 -0800

    Weather picker test

--- main.js
+++ main.js
@@ -4,35 +4,32 @@ const bodyParser = require('body-parser');
 const argv = require('yargs').argv;
 const app = express();
 
-let apiKey = 'bedaf9a789a504a64aff26996089ef2b';
-let city = argv.c || `rocklin`;
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-
-request(url, function (err, response, body) {
-    if(err){
-        console.log('error:', error);
-    } else {
-        let weather = JSON.parse(body);
-        let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        console.log(message);
-    }
-});
-
-let use = app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {weather: null, error: null});
 });
 
 app.post('/', function (req, res) {
-    res.render('index');
-    console.log(req.body.city)
-});
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    request(url, function (err, response, body) {
+        if (err) {
+            console.log('error:', error);
+        } else {
+            let weather = JSON.parse(body);
+            let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            console.log(message);
+        }
+    });
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!')
+
+    });
 
 }); 
 
