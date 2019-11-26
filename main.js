ec55948de186a7c122dfd5885ef34f57a2d34c92
Cecily Kerns <cecilyrose2002@gmail.com>
2019-11-25 16:47:51 -0800

    Bug fixes

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
 app.post('/', function (req, res) {
     res.render('index');
     console.log(req.body.city)
