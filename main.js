c6185dfd514896aeaf619364abb9f68b326f1fc9
Cecily Kerns <cecilyrose2002@gmail.com>
2019-11-27 13:02:21 -0800

    Adding in cache functionality

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+let mcache=require('memory-cache');
 const app = express();
 
 const apiKey = 'bedaf9a789a504a64aff26996089ef2b';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs', 'jade');
 
-app.get('/', function (req, res) {
+let cache = (duration) => {
+    return (req, res, next) => {
+        let key = '__express__' + req.originalUrl || req.url;
+        let cachedBody = mcache.get(key);
+        if (cachedBody) {
+            res.send(cachedBody);
+        } else {
+            res.sendResponse = res.send;
+            res.send = (body) => {
+                mcache.put(key, body, duration * 1000);
+                res.sendResponse(body)
+            };
+            next()
+        }
+    }
+};
+
+app.get('/', cache(10), function (req, res) {
     res.render('index', {weather: null, error: null});
 });
 
@@ -18,58 +36,20 @@ app.post('/', function (req, res) {
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     request(url, function (err, response, body) {
-        if (!err) {
+        if(err){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
             let weather = JSON.parse(body);
-            if (weather.main === undefined) {
+            if(weather.main === undefined){
                 res.render('index', {weather: null, error: 'Error, please try again'});
             } else {
-                let temperature = weather.main.temp;
-                let weatherCity = weather.name;
-
-                let weatherData = [
-                    {weatherCity, temperature}
-                ];
-
-                function generateTableHead(table, data) {
-                    let thead = table.createTHead();
-                    let row = thead.insertRow();
-                    for (let key of data) {
-                        let th = document.createElement("th");
-                        let text = document.createTextNode(key);
-                        th.appendChild(text);
-                        row.appendChild(th);
-                    }
-                }
-
-                function generateTable(table, data) {
-                    for (let element of data) {
-                        let row = table.insertRow();
-                        for (let key in element) {
-                            let cell = row.insertCell();
-                            let text = document.createTextNode(element);
-                            cell.appendChild(text);
-                        }
-                    }
-                }
-
-                let table = document.querySelector("table");
-                let data = Object.keys(weatherData[0]);
-                generateTableHead(table, data);
-                generateTable(table, weatherData);
-            }
-        } else res.render('index', {weather: null, error: 'Error, please try again'});
-    });
-
-
-    let weatherText = `It's ${temperature} degrees Fahrenheit in ${weatherCity}!`;
+                let weatherText = `It's ${weather.main.temp} degrees Fahrenheit in ${weather.name}!`;
                 res.render('index', {weather: weatherText, error: null});
-
+            }
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 });
-});
-
-
-
-
