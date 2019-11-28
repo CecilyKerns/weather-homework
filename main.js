ff9ae2e0b3b2f21ddfdf5b77a8fcd888f6921355
Cecily Kerns <cecilyrose2002@gmail.com>
2019-11-27 16:19:31 -0800

    Finishing up cache and doing HTML customization

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-let mcache=require('memory-cache');
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
 const app = express();
 
 const apiKey = 'bedaf9a789a504a64aff26996089ef2b';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs', 'jade');
+app.set('view engine', 'ejs');
 
-let cache = (duration) => {
-    return (req, res, next) => {
-        let key = '__express__' + req.originalUrl || req.url;
-        let cachedBody = mcache.get(key);
-        if (cachedBody) {
-            res.send(cachedBody);
-        } else {
-            res.sendResponse = res.send;
-            res.send = (body) => {
-                mcache.put(key, body, duration * 1000);
-                res.sendResponse(body)
-            };
-            next()
-        }
-    }
-};
-
-app.get('/', cache(10), function (req, res) {
+app.get('/', function (req, res) {
     res.render('index', {weather: null, error: null});
 });
 
@@ -35,21 +19,25 @@ app.post('/', function (req, res) {
     let city = req.body.city;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-    request(url, function (err, response, body) {
+    request(url,function (err, response, body) {
         if(err){
             res.render('index', {weather: null, error: 'Error, please try again'});
         } else {
             let weather = JSON.parse(body);
+
             if(weather.main === undefined){
                 res.render('index', {weather: null, error: 'Error, please try again'});
             } else {
                 let weatherText = `It's ${weather.main.temp} degrees Fahrenheit in ${weather.name}!`;
                 res.render('index', {weather: weatherText, error: null});
+
+                myCache.set("weatherTemperatureKey", weather.main.temp, 1800);
+                myCache.set("weatherTemperatureKey", weather.name, 1800);
             }
         }
     });
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 });
